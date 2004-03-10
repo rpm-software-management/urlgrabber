baaaa1ca49a811fd635e973e6457b0ba6ba3f4ea
rtomayko <rtomayko>
2004-03-10 06:08:42 +0000

    Fixed up for new cvs structure. Tried to simplify a bit while I was here.

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from distutils.core import setup
-setup(name="urlgrabber",
-      version="0.3",
-      description="high-level cross-protocol url-grabber",
-      author="Michael D. Stenner, Ryan Tomayko",
-      author_email="mstenner@phy.duke.edu, rtomayko@naeblis.cx",
-      url="http://linux.duke.edu/projects/mini/urlgrabber/",
-      license="GPL",
-      packages=['URLGrabber'],
-      package_dir={'URLGrabber' : ''},
-      scripts=['scripts/urlgrabber'],
-      data_files=[
-          ('share/doc', ['README','LICENSE', 'TODO'])
-          ],
-      options = { 
-        'clean' : { 'all' : 1 }
-        },
-      )
+# urlgrabber distutils setup
+
+name = "urlgrabber"
+version = "0.3"
+description = "high-level cross-protocol url-grabber"
+author = "Michael D. Stenner, Ryan Tomayko"
+author_email = "mstenner@phy.duke.edu, rtomayko@naeblis.cx"
+url = "http://linux.duke.edu/projects/mini/urlgrabber/"
+license="GPL"
+packages = ['urlgrabber']
+package_dir = {'urlgrabber':'urlgrabber'}
+scripts = ['scripts/urlgrabber']
+data_files = [('share/doc/' + name + '-' + version, ['README','LICENSE', 'TODO', 'ChangeLog'])]
+options = { 'clean' : { 'all' : 1 } }
+
+# load up distutils
+if __name__ == '__main__':
+  config = globals().copy()
+  del config['__builtins__']
+  del config['__name__']
+  
+  from distutils.core import setup
+  setup(**config)
