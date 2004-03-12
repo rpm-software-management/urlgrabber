f2f3e91e1b4cffa061868be8b8acc5c7364dbff8
rtomayko <rtomayko>
2004-03-12 03:21:18 +0000

    Added mirror tests to main test suite.

--- test/runtests.py
+++ test/runtests.py
@@ -11,8 +11,10 @@ def main():
     dn = dirname(sys.argv[0])
     sys.path.insert(0, joinpath(dn,'..'))
     sys.path.insert(0, dn)
-    import test_grabber, test_byterange
-    suite = unittest.TestSuite( (test_grabber.suite(),test_byterange.suite()) )
+    import test_grabber, test_byterange, test_mirror
+    suite = unittest.TestSuite( (test_grabber.suite(),
+                                 test_byterange.suite(), 
+                                 test_mirror.suite()) )
     runner = unittest.TextTestRunner(descriptions=1,verbosity=2)
     runner.run(suite)
     
