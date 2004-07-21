ce16115bc4eb9bbdd784298ada47ef6638557738
rtomayko <rtomayko>
2004-07-21 02:47:49 +0000

    Added distutils trove classifiers to setup.py for eventual inclusion in PyPI. Added general description string for urlgrabber package (__init__.py).

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ import re as _re
 import urlgrabber as _urlgrabber
 
 name = "urlgrabber"
-description = "high-level cross-protocol url-grabber"
-license = "GPL"
+description = "A high-level cross-protocol url-grabber"
+long_description = _urlgrabber.__doc__
+license = "LGPL"
 version = _urlgrabber.__version__
 _authors = _re.split(r',\s+', _urlgrabber.__author__)
 author       = ', '.join([_re.sub(r'\s+<.*',        r'', _) for _ in _authors])
@@ -17,6 +18,20 @@ scripts = ['scripts/urlgrabber']
 data_files = [('share/doc/' + name + '-' + version,
                ['README','LICENSE', 'TODO', 'ChangeLog'])]
 options = { 'clean' : { 'all' : 1 } }
+classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Internet :: File Transfer Protocol (FTP)',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
 
 # load up distutils
 if __name__ == '__main__':
