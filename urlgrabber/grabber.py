d27763d1cb035f457b066195bdc86db4055ecef7
Seth Vidal <skvidal@fedoraproject.org>
2010-02-19 15:03:29 -0500

    add reset_curl_obj() to top layer of urlgrabber.grabber to close and reopen the curl object in the event the network info changes a lot.
    marginally improve the error output for a bogus cert

--- urlgrabber/grabber.py
+++ urlgrabber/grabber.py
@@ -1299,7 +1299,7 @@ class PyCurlFileObject():
                 raise err
 
             elif errcode == 60:
-                msg = _("client cert cannot be verified or client cert incorrect")
+                msg = _("Peer cert cannot be verified or peer cert invalid")
                 err = URLGrabError(14, msg)
                 err.url = self.url
                 raise err
@@ -1640,6 +1640,14 @@ class PyCurlFileObject():
         
 _curl_cache = pycurl.Curl() # make one and reuse it over and over and over
 
+def reset_curl_obj():
+    """To make sure curl has reread the network/dns info we force a reload"""
+    global _curl_cache
+    _curl_cache.close()
+    _curl_cache = pycurl.Curl()
+
+
+    
 
 #####################################################################
 # DEPRECATED FUNCTIONS
