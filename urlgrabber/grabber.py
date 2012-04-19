26f05e195a9641778e47f63f83bc2dd9822a6a73
Zdeněk Pavlas <zpavlas@redhat.com>
2012-04-19 17:20:03 +0200

    use proxy='' to disable default proxies
    Instead of ignoring proxy settings '_none_' or '',
    use it to disable default proxies in libcurl.

--- urlgrabber/grabber.py
+++ urlgrabber/grabber.py
@@ -830,8 +830,9 @@ class URLGrabberOptions:
                     proxy = self.proxies.get('https')
                 elif scheme == 'https':
                     proxy = self.proxies.get('http')
-            if proxy != '_none_':
-                self.proxy = proxy
+            if proxy == '_none_':
+                proxy = ''
+            self.proxy = proxy
             return
 
         if self.libproxy:
@@ -1318,7 +1319,7 @@ class PyCurlFileObject(object):
             self.curl_obj.setopt(pycurl.MAX_RECV_SPEED_LARGE, int(opts.raw_throttle()))
             
         # proxy
-        if opts.proxy:
+        if opts.proxy is not None:
             self.curl_obj.setopt(pycurl.PROXY, opts.proxy)
             self.curl_obj.setopt(pycurl.PROXYAUTH, pycurl.HTTPAUTH_ANY)
 
