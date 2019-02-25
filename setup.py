492df4d0e00c19334e39541e5a8167dceb3b4b55
Neal Gompa <ngompa13@gmail.com>
2019-02-25 07:21:56 -0500

    Fix setup.py to use setuptools and have correct metadata

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ del _sys.modules["pycurl"]
 name = "urlgrabber"
 description = "A high-level cross-protocol url-grabber"
 long_description = _urlgrabber.__doc__
-license = "LGPL"
+license = "LGPLv2+"
 version = _urlgrabber.__version__
 _authors = _re.split(r',\s+', _urlgrabber.__author__)
 author       = ', '.join([_re.sub(r'\s+<.*',        r'', _) for _ in _authors])
@@ -35,6 +35,7 @@ data_files = [
     ('share/doc/' + name + '-' + version, ['README','LICENSE', 'TODO', 'ChangeLog']),
     ('libexec', ['scripts/urlgrabber-ext-down']),
 ]
+install_requires = ['pycurl']
 options = { 'clean' : { 'all' : 1 } }
 classifiers = [
         'Development Status :: 4 - Beta',
@@ -42,7 +43,7 @@ classifiers = [
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Operating System :: POSIX',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
@@ -59,5 +60,5 @@ if __name__ == '__main__':
     #print '%-20s -> %s' % (k, config[k])
     if k.startswith('_'): del config[k]
 
-  from distutils.core import setup
+  from setuptools import setup
   setup(**config)
