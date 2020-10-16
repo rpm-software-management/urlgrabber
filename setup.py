1fe96a19fb62a61bf0c3d3738f7e08bced36cf89
John Vandenberg <jayvdb@gmail.com>
2020-10-16 11:45:00 +0700

    setup.py: Update url
    http://urlgrabber.baseurl.org/ doesnt link to the current SCM

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    url="http://urlgrabber.baseurl.org/",
+    url="https://github.com/rpm-software-management/urlgrabber",
     author="Michael D. Stenner, Ryan Tomayko, Seth Vidal, Zdenek Pavlas",
     author_email="mstenner@linux.duke.edu, rtomayko@naeblis.cx, skvidal@fedoraproject.org, zpavlas@redhat.com",
     maintainer="Neal Gompa",
